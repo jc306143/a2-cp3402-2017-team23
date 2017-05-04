09c086bfe13e1feeee95c43ab0e990fd360f31f6
Martin Wilkinson <martin.wilkinson@my.jcu.edu.au>
2017-05-04 11:29:43 +1000

    underscores basic theme commit

---
+++ themes/team23theme/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Team23Theme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'team23theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'team23theme' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'team23theme' ), 'team23theme', '<a href="https://automattic.com/" rel="designer">team23</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
