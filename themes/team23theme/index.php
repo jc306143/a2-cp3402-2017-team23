f68286663da7f5367baa93a5abd72d61afa52b3e
Bruce <brucemaddison96@gmail.com>
2017-05-11 11:10:17 +1000

    Customised Index, Archive, and Search Templates

--- themes/team23theme/index.php
+++ themes/team23theme/index.php
@@ -14,11 +14,12 @@
 
 get_header(); ?>
 
+<?php if ( have_posts() ) : ?>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php
-		if ( have_posts() ) :
 
 			if ( is_home() && ! is_front_page() ) : ?>
 				<header>
@@ -40,13 +41,13 @@ get_header(); ?>
 
 			endwhile;
 
-			the_posts_navigation();
+			the_posts_pagination( array(
+				'prev_text' => __( 'Newer', 'humescores' ),
+				'next_text' => __( 'Older', 'humescores' ),
+				'before_page_number' => '<span class="screen-reader-text">' . __( 'Page ', 'humescores' ) . '</span>',
+			));
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
+		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
@@ -54,3 +55,11 @@ get_header(); ?>
 <?php
 get_sidebar();
 get_footer();
+
+
+else :
+
+	get_template_part( 'template-parts/content', 'none' );
+	return;
+
+endif;
